af514993989827bf0a962d1b72d755a9d6e285a5
DaveloperT <davide.tamburrino@gmail.com>
2023-10-09 16:33:29 +0200

    Update App.java

--- App.java
+++ App.java
@@ -18,7 +18,7 @@ public class App
         
     }
 
-    public static void execute(String inputString) throws UserInputException {
+    public static void execute(String inputString) throws UserInputException{
         MakeBusiness mb = new MakeBusiness();
         mb.makeBusiness(inputString);
     }
